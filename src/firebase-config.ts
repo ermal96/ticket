0cee7053741aec26f24ac112ad1536f18491cb87
Ermal Vrapi <ermalvrapi18@gmail.com>
2023-02-25 10:49:26 +0100

    fix: add firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDH0xMT8BzBuIlg10fdxFkT8PeUWGhZUpo",
+    authDomain: "lufthansa-6a0a1.firebaseapp.com",
+    projectId: "lufthansa-6a0a1",
+    storageBucket: "lufthansa-6a0a1.appspot.com",
+    messagingSenderId: "553731870710",
+    appId: "1:553731870710:web:0a92cc3a456d56adf9c1d7"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
