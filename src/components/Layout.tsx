b975f2eaed3e0eb897d3dad825d59d9428cfdcc6
Ermal Vrapi <ermalvrapi18@gmail.com>
2023-02-27 15:59:10 +0100

    fix: add layout with children props & Header

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@mui/material";
+import Header from "./Header";
+
+type Props = {
+    children: JSX.Element
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <Container maxWidth="md">
+            <Header />
+            {children}
+        </Container>
+    )
+}
+
+export default Layout;
