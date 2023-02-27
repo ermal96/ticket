d04839ece6760f4436ea700e77e1c5fdcf8fed53
Ermal Vrapi <ermalvrapi18@gmail.com>
2023-02-27 16:01:36 +0100

    fix: update types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,28 @@
 export type UserRole = 'ADMIN' | 'USER';
+
+export type UserAuth = {
+    email: string,
+    password: string,
+    role?: UserRole
+}
+
+export type AuthState = {
+    loading: boolean,
+    loggedIn: boolean,
+    error?: string,
+    user?: {
+        email: string,
+        role?: UserRole
+    }
+
+}
+
+export type FirebaseAuthResponse = {
+    email: string,
+    role?: UserRole
+}
+
+export type FirebaseError = {
+    code: number,
+    message: string
+}
