0508c04816576ddd8f7d1848936347abddca74e9
Ermal Vrapi <ermalvrapi18@gmail.com>
2023-02-28 17:04:19 +0100

    fix: add ticket type

--- src/types.ts
+++ src/types.ts
@@ -26,3 +26,23 @@ export type FirebaseError = {
     code: number,
     message: string
 }
+
+export type TicketType = "ECONOMY" | "BUSINESS";
+
+export type Ticket = {
+    id?: string,
+    ticket_type_id: string,
+    inbound: string,
+    outbound: string,
+    ticket_type: TicketType
+    price: number,
+    from_date: string,
+    to_date: string,
+    seat_number: number
+}
+
+export type TicketState = {
+    loading: boolean,
+    error?: string,
+    tickets?: Ticket[]
+}
